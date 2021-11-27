7fea2844809d43950b5b8688c216274cd16eff40
jinmae1 <jinmae1@gmail.com>
2021-11-27 17:46:37 +0900

    Bulletin dao, vo, service 추가

---
+++ src/main/java/com/otlb/semi/bulletin/model/BoardEntity.java
@@ -0,0 +1,34 @@
+package com.otlb.semi.bulletin.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import com.otlb.semi.bulletin.Bulletin;
+
+public class BoardEntity extends Bulletin implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int likeCount;
+	private String reportYn;
+	private String category;
+
+	public BoardEntity() {
+		super();
+	}
+
+	public BoardEntity(int no, String title, String content, Date regDate, int readCount, int likeCount, String reportYn, int userNo, String category, String deleteYn) {
+		super(no, title, content, regDate, readCount, userNo, deleteYn);
+		this.likeCount = likeCount;
+		this.reportYn = reportYn;
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "BoardEntity [likeCount=" + likeCount + ", reportYn=" + reportYn + ", category=" + category + "]";
+	}
+
+	
+	
+}
