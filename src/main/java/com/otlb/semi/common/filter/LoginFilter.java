205efcf1a153ea0870107275813ce334c381e151
leehb105 <leesonge95@gmail.com>
2021-12-05 00:03:09 +0900

    로그인 필터 생성

---
+++ src/main/java/com/otlb/semi/common/filter/LoginFilter.java
@@ -0,0 +1,64 @@
+package com.otlb.semi.common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.otlb.semi.emp.model.vo.Emp;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/message/*")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		//로그인여부 검사
+		HttpSession session = httpRequest.getSession();
+		Emp loginEmp = (Emp) session.getAttribute("loginEmp");
+		
+		if(loginEmp == null) {
+			session.setAttribute("msg", "로그인이 필요한 페이지 입니다.");
+			httpResponse.sendRedirect(httpRequest.getContextPath() + "/");
+			return;
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
