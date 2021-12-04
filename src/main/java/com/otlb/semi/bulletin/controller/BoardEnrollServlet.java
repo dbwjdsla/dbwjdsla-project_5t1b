384dc825daf5a343d35bd3f3fb87c2f7940d4919
hyunha95 <yaa4500@naver.com>
2021-12-05 00:52:37 +0900

    게시글 등록 성공

--- src/main/java/com/otlb/semi/bulletin/controller/BoardEnrollServlet.java
+++ src/main/java/com/otlb/semi/bulletin/controller/BoardEnrollServlet.java
@@ -7,21 +7,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.otlb.semi.bulletin.model.service.BulletinService;
+import com.otlb.semi.bulletin.model.vo.Board;
+
 /**
  * Servlet implementation class BoardEnrollServlet
  */
-@WebServlet("/board/boardForm")
+@WebServlet("/board/boardEnroll")
 public class BoardEnrollServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private BulletinService bulletinService = new BulletinService();
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * insert into board(no, category, title, content) values(?, ?, ?, ?)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request
-			.getRequestDispatcher("/WEB-INF/views/board/boardForm.jsp")
-			.forward(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 사용자입력값 처리
+		String category = request.getParameter("category");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		int empNo = Integer.parseInt(request.getParameter("empNo"));
 		
+		Board board = new Board();
+		board.setCategory(category);
+		board.setTitle(title);
+		board.setContent(content);
+		board.setEmpNo(empNo);
+		
+		// 2. 업무로직
+		int result = bulletinService.insertBoard(board);
+		String msg = result > 0 ? "게시물 등록 성공" : "게시물 등록 실패";
+		
+		// 3. 응답요청
+		request.getSession().setAttribute("msg", msg);
+		String location = request.getContextPath() + "/board/boardForm";
+		response.sendRedirect(location);
 	}
 
 }
--- src/main/java/com/otlb/semi/bulletin/model/service/BulletinService.java
+++ src/main/java/com/otlb/semi/bulletin/model/service/BulletinService.java
@@ -2,7 +2,10 @@ package com.otlb.semi.bulletin.model.service;
 
 import static com.otlb.semi.common.JdbcTemplate.*;
 
+import java.sql.Connection;
+
 import com.otlb.semi.bulletin.model.dao.BulletinDao;
+import com.otlb.semi.bulletin.model.vo.Board;
 
 public class BulletinService {
 
@@ -12,4 +15,21 @@ public class BulletinService {
 	public static final String isNotReported = "N";
 	
 	private BulletinDao bulletinDao = new BulletinDao();
+
+	public int insertBoard(Board board) {
+		Connection conn = null;
+		int result = 0;
+		
+		try {
+			conn = getConnection();
+			result = bulletinDao.insertBoard(conn, board);
+			commit(conn);
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+		return result;
+	}
 }
