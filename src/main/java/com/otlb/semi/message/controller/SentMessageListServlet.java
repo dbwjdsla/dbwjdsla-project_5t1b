491a12a0cae4ac28e30ef3f57fd974d2c1ef0140
leehb105 <leesonge95@gmail.com>
2021-12-05 00:03:02 +0900

    쪽지함 날짜 포맷 적용

--- src/main/java/com/otlb/semi/message/controller/SentMessageListServlet.java
+++ src/main/java/com/otlb/semi/message/controller/SentMessageListServlet.java
@@ -1,6 +1,7 @@
 package com.otlb.semi.message.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.otlb.semi.common.DateFormatUtils;
 import com.otlb.semi.emp.model.vo.Emp;
 import com.otlb.semi.message.model.service.MessageService;
 import com.otlb.semi.message.model.vo.Message;
@@ -33,9 +35,17 @@ public class SentMessageListServlet extends HttpServlet {
 		
 		int empNo = emp.getEmpNo();
 		List<Message> list = messageService.selectAllSentMessage(empNo);
-		System.out.println("[MessageListServlet] list = " + list);
+		//System.out.println("[MessageListServlet] list = " + list);
+		List<String> sentDateList = new ArrayList<>();
+		List<String> readDateList = new ArrayList<>();
 		
+		for(int i = 0; i < list.size(); i++) {
+			sentDateList.add(DateFormatUtils.formatDate(list.get(i).getSentDate()));
+			readDateList.add(DateFormatUtils.formatDate(list.get(i).getReadDate()));
+		}
 		request.setAttribute("list", list);
+		request.setAttribute("sentDateList", sentDateList);
+		request.setAttribute("readDateList", readDateList);
 		request
 			.getRequestDispatcher("/WEB-INF/views/message/sentMessageList.jsp")
 			.forward(request, response);
