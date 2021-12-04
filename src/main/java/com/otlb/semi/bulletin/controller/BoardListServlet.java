3da1039fc07ab0ab6b9a48b9383afd13c54b1287
hyunha95 <yaa4500@naver.com>
2021-12-04 15:31:01 +0900

    게시판 관련 링크 임시로 연결

---
+++ src/main/java/com/otlb/semi/bulletin/controller/BoardListServlet.java
@@ -0,0 +1,26 @@
+package com.otlb.semi.bulletin.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardListServlet
+ */
+@WebServlet("/board/boardList")
+public class BoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request
+			.getRequestDispatcher("/WEB-INF/views/board/boardList.jsp")
+			.forward(request, response);
+	}
+
+}
