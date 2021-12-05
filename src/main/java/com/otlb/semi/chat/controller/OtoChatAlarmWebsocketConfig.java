0a9bb93ce7e34dd093f0597619004a93c78c3e2a
SOYOUNG <sysy931207@gmail.com>
2021-12-05 16:54:09 +0900

    채팅 기능 추가 (현재 회원 정보 모달 완성된게 없어서 하드코딩 해 놓은 상태 완성되는대로 수정 예정)

--- src/main/java/com/otlb/semi/chat/controller/OtoChatAlarmWebsocketConfig.java
+++ src/main/java/com/otlb/semi/chat/controller/OtoChatAlarmWebsocketConfig.java
@@ -17,13 +17,19 @@ public class OtoChatAlarmWebsocketConfig extends Configurator {
 		//	
 		HttpSession session = (HttpSession) request.getHttpSession();
 		Emp	ssEmp	=	(Emp)session.getAttribute("loginEmp"); 
+
+		Map<String, Object> userProp = sec.getUserProperties();
 		
 		if(ssEmp != null) {
 			String userId	=	""+ssEmp.getEmpNo();
 			
 			// config 
-			Map<String, Object> userProp = sec.getUserProperties();
-			userProp.put("userId", userId);
+			userProp.put("userId", userId);  // 현재 사용자 아이디 
+			userProp.put("loginYn", "Y");    // 로그인 여부 
+		}else {
+			// 로그인 아닌경우는 로그인 아님을 알려준다 
+			userProp.put("userId", "");      // 현재 사용자 아이디 
+			userProp.put("loginYn", "N");    // 로그인 여부 
 		}
 	}
 	
