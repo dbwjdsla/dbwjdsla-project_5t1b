e6f0a8aba6a8823bb6b99658d4f4622c3b8e6207
uuuuj <yoojeong0410@gmail.com>
2021-12-07 00:37:43 +0900

    공지사항 servlet

--- src/main/java/com/otlb/semi/bulletin/controller/NoticeListServlet.java
+++ src/main/java/com/otlb/semi/bulletin/controller/NoticeListServlet.java
@@ -1,23 +1,35 @@
 package com.otlb.semi.bulletin.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.otlb.semi.bulletin.model.service.BulletinService;
+import com.otlb.semi.bulletin.model.vo.Notice;
+
 /**
  * Servlet implementation class NoticeListServlet
  */
-@WebServlet("/borad/noticeList")
+@WebServlet("/notice/noticeList")
 public class NoticeListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private BulletinService bulletinService = new BulletinService();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Notice> list = bulletinService.selectAllNotice();
+		
+		request.setAttribute("list", list);
+
+		
 		request
 			.getRequestDispatcher("/WEB-INF/views/notice/noticeList.jsp")
 			.forward(request, response);
